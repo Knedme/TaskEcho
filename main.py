0661ad8cf141378ec338a80b6f8e4265ef999874
knedme <knedme1@gmail.com>
2024-08-30 09:03:11 +0300

    empty qt program created

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Hello...')
+import sys
+from PyQt6.QtWidgets import QWidget, QApplication
+
+
+class MainWindow(QWidget):
+    
+    def __init__(self):
+        super().__init__()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
