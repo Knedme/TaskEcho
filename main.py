54517ebcb1e006be63bdae604d3f0e736d8005da
knedme <knedme1@gmail.com>
2024-08-30 10:21:39 +0300

    hello world!!

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import sys
 from PyQt6.QtWidgets import QWidget, QApplication
+from PyQt6 import uic
 
 
 class MainWindow(QWidget):
     
     def __init__(self):
         super().__init__()
+        uic.loadUi('./ui/main.ui', self)
 
 
 if __name__ == '__main__':
