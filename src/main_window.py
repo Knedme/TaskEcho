457ddac4c3fe4a23faa866c7e1ed856181c34e79
knedme <knedme1@gmail.com>
2024-09-08 08:17:12 +0300

    i didnt test it...

--- src/main_window.py
+++ src/main_window.py
@@ -175,11 +175,11 @@ class MainWindow(QMainWindow):
                             mouse.scroll(*args)
                         case ActionType.K_PRESS:
                             key = args[0]
-                            software_cancel_press = key.vk == cancel_key
+                            software_cancel_press = hasattr(key, 'vk') and key.vk == cancel_key
                             keyboard.press(key)
                         case ActionType.K_RELEASE:
                             key = args[0]
-                            software_cancel_press = key.vk == cancel_key
+                            software_cancel_press = hasattr(key, 'vk') and key.vk == cancel_key
                             keyboard.release(key)
             cancel_listener.stop()  # Stop listening to cancel key presses
             on_end()  # Call on_end when the loop is over
@@ -187,7 +187,7 @@ class MainWindow(QMainWindow):
         # Listen to cancel key presses
         def detect_cancel(key):
             nonlocal software_cancel_press, loop_cancelled
-            if key.vk == cancel_key and not software_cancel_press:
+            if hasattr(key, 'vk') and key.vk == cancel_key and not software_cancel_press:
                 loop_cancelled = True
                 on_end()
                 return False
